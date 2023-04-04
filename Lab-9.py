2fa895c380760527eaf4676205e7abfe2db80351
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-04-04 14:48:26 -0400

    Updated script

--- Lab-9.py
+++ Lab-9.py
@@ -38,14 +38,15 @@ def handle_get_info():
     
     
     types_list = [t['type']['name'].title() for t in poki_info['types']]
-    n = len(types_list)
-    # Checks the length of the abilities and then prints it
-    if n == 1:
-        lbl_type_true['text'] = f"{types_list[0]}"
-    
+    type = len(types_list)
+    if type == 1:
+        lbl_type_value ['text'] = f"{types_list[0]}"
     else:
-        types_csl = ', '.join(types_list[:-1])
-        lbl_type_true['text'] = f"{types_csl}, {types_list[-1]}"
+         types2 = ', '.join(types_list[:-1])
+         lbl_type_value['text'] = f"{types2}, {types_list[-1]}"
+        
+    
+    
     
     # displays the stats 
     bar_hp['value'] = poki_info['stats'][0]['base_stat']
@@ -58,66 +59,68 @@ def handle_get_info():
 
 # Populaate widget in the info frame
 btn_get_info = ttk.Button(frm_top, text='Get Info', command=handle_get_info)
-btn_get_info.grid(row=0, column=2, padx= 10, pady= 10)
+btn_get_info.grid(row=0, column=2, padx= 10, pady= 10, )
 
 
 # Adds the weight value 
 lbl_height = ttk.Label (frm_btm_left, text='Height:')
-lbl_height.grid(row=0, column=0)
+lbl_height.grid(row=0, column=0, padx=(10,5), pady=(10,5), sticky=E)
+
 lbl_height_value = ttk.Label(frm_btm_left, text= 'TBD')
-lbl_height_value.grid(row=0, column=1)
+lbl_height_value.grid(row=0, column=1, padx=(0,10), pady=(10,5), sticky=W)
 
 
 # Adds the height value 
 lbl_weight = ttk.Label (frm_btm_left, text='Weight:')
-lbl_weight.grid(row=1, column=0)
+lbl_weight.grid(row=1, column=0, padx=(10,5), pady=(10,5), sticky=E)
+
 lbl_weight_value = ttk.Label(frm_btm_left, text= 'TBD')
-lbl_weight_value.grid(row=1, column=1)
+lbl_weight_value.grid(row=1, column=1, padx=(0,10), pady=(10,5), sticky=W)
 
 
 # Pokemon type info
 lbl_type = ttk.Label(frm_btm_left, text='Type:')
-lbl_type.grid(row=2, column=0, padx=(10, 5), pady=(10, 5), )
+lbl_type.grid(row=2, column=0, padx=(10, 5), pady=(10, 5), sticky=E)
 
-lbl_type_true = ttk.Label(frm_btm_left, text='TBD')
-lbl_type_true.grid(row=2, column=1, padx=(0, 10), pady=(10,5))
+lbl_type_value = ttk.Label(frm_btm_left, text='TBD')
+lbl_type_value.grid(row=2, column=1, padx=(0, 10), pady=(10,5), sticky=W)
 
 
 #Adds the hp
 lbl_hp = ttk.Label(frm_btm_right, text= 'HP:')
-lbl_hp.grid(row=0, column=0)
+lbl_hp.grid(row=0, column=0, sticky=E, padx=(10,5), pady=(10,5))
 bar_hp = ttk.Progressbar(frm_btm_right, orient= HORIZONTAL, length=200, maximum=255)
-bar_hp.grid(row=0 , column=1)
+bar_hp.grid(row=0 , column=1, padx=(0,10), pady=5)
 
 # Adds the attack
 lbl_attack = ttk.Label(frm_btm_right, text= 'Attack:')
-lbl_attack.grid(row=1, column=0)
+lbl_attack.grid(row=1, column=0, sticky=E, padx=(10,5), pady=5)
 bar_attack = ttk.Progressbar(frm_btm_right, orient= HORIZONTAL, length=200, maximum=255)
-bar_attack.grid(row=1 , column=1)
+bar_attack.grid(row=1 , column=1, padx=(0,10), pady=5)
 
 # Adds the defense
-lbl_defense = ttk.Label(frm_btm_right, text= 'Defense')
-lbl_defense.grid(row=2, column=0)
+lbl_defense = ttk.Label(frm_btm_right, text= 'Defense:')
+lbl_defense.grid(row=2, column=0, sticky=E, padx=(10,5), pady=(10,5))
 bar_defense = ttk.Progressbar(frm_btm_right, orient= HORIZONTAL, length=200, maximum=255)
-bar_defense.grid(row=2, column=1)
+bar_defense.grid(row=2, column=1, padx=(0,10), pady=5)
 
 # Adds Special attack
-lbl_special_attack=ttk.Label(frm_btm_right, text= 'Special Attack')
-lbl_special_attack.grid(row=3, column=0)
+lbl_special_attack=ttk.Label(frm_btm_right, text= 'Special Attack:')
+lbl_special_attack.grid(row=3, column=0, sticky=E, padx=(10,5), pady=(10,5))
 bar_Special_attack = ttk.Progressbar(frm_btm_right, orient= HORIZONTAL, length=200, maximum=255)
-bar_Special_attack.grid(row=3, column=1)
+bar_Special_attack.grid(row=3, column=1, padx=(0,10), pady=5)
 
 # Adds Special defense
-lbl_special_Defense=ttk.Label(frm_btm_right, text= 'Special Defense')
-lbl_special_Defense.grid(row=4, column=0)
+lbl_special_Defense=ttk.Label(frm_btm_right, text= 'Special Defense:')
+lbl_special_Defense.grid(row=4, column=0, sticky=E, padx=(10,5), pady=(10,5))
 bar_Special_Defense = ttk.Progressbar(frm_btm_right, orient= HORIZONTAL, length=200, maximum=255)
-bar_Special_Defense.grid(row=4, column=1)
+bar_Special_Defense.grid(row=4, column=1, padx=(0,10), pady=5)
 
 # Adds Speed
-lbl_speed=ttk.Label(frm_btm_right, text= 'Speed')
-lbl_speed.grid(row=5, column=0)
+lbl_speed=ttk.Label(frm_btm_right, text= 'Speed:')
+lbl_speed.grid(row=5, column=0, sticky=E, padx=(10,5), pady=(10,5))
 bar_speed = ttk.Progressbar(frm_btm_right, orient= HORIZONTAL, length=200, maximum=255)
-bar_speed.grid(row=5, column=1)
+bar_speed.grid(row=5, column=1, padx=(0,10), pady=5)
 
 
 #loop until window is closed 
